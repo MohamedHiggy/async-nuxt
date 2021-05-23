70735dad5d53cd8484f9cf616e4fcd8b033ed5c9
hagooog <mohamed.higgy31@gmail.com>
2021-05-23 18:00:28 +0200

    add vuelidate plugin and refactor code

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
     },
 
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-    plugins: ['~/plugins/axios'],
+    plugins: ['~/plugins/axios', { src: '~/plugins/vuelidate.js', mode: 'both', }],
 
     // Auto import components (https://go.nuxtjs.dev/config-components)
     components: true,
--- pages/form.vue
+++ pages/form.vue
@@ -5,13 +5,13 @@
       <div class="form-group">
         <label for="name">Name</label>
         <input type="text" v-model="form.name" class="form-control" id="name" placeholder="Enter your name">
-        <small id="name" class="form-text text-danger" v-if="!nameIsValid">the name filed is required</small>
+        <small id="name" class="form-text text-danger" v-if="$v.form.name.$invalid">the name filed is required</small>
       </div>
 
       <div class="form-group">
         <label for="age">Age</label>
         <input type="text" v-model.number="form.age" class="form-control" id="age" placeholder="Enter your age">
-        <small id="age" class="form-text text-danger" v-if="!ageIsValid">the age must be at least 12 and max 100</small>
+        <small id="age" class="form-text text-danger" v-if="$v.form.age.$invalid">the age must be at least 12 and max 100</small>
       </div>
 
       <div class="form-check">
@@ -19,13 +19,14 @@
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
       </div>
 
-      <button type="submit" :disabled="!formsIsValid" class="btn btn-primary btn-lg btn-block mt-3">Submit</button>
+      <button type="submit" :disabled="$v.form.$invalid" class="btn btn-primary btn-lg btn-block mt-3">Submit</button>
       <pre>{{form}}</pre>
   </form>
 </div>
 </template>
 
 <script>
+import { required, numeric, minValue, maxValue, between } from 'vuelidate/lib/validators'
   export default {
     data() {
       return {
@@ -36,20 +37,22 @@
         }
       }
     },
-    computed: {
-      nameIsValid(){
-        return !!this.form.name
+    computed: {},
+    validations: {
+      form: {
+        name: {
+          required,
+        },
+        age: {
+          required,
+          numeric,
+          between: between(15, 100),
+        },
       },
-      ageIsValid(){
-        return typeof this.form.age === "number" && this.form.age > 12 && this.form.age < 100;
-      },
-      formsIsValid() {
-        return this.nameIsValid && this.ageIsValid;
-      }
     },
     methods: {
       submitFrom() {
-        if (this.formsIsValid) {
+        if (!this.$v.form.$invalid) {
           console.log(this.form);
         }
         else {
